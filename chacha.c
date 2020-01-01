c4e4459a08d4d30fe0419a17b66c082e91425997
Gavin <gavinbrrtt@gmail.com>
2020-01-01 15:26:28 -0800

    correctly producing test vector message tag; desperately need to clean up code and refactor functions

--- chacha.c
+++ chacha.c
@@ -41,43 +41,35 @@ void print_bytestream(uint32_t* keystream) {
 
 void print_charstream(unsigned char* st, size_t sz) {
 	/* print the ChaCha key or cipher stream as an array of chars */
-	for (int j = 0; j < (int)sz; j++) {
+	for (int j = 0; j < (int)sz; j++)
 		printf("%c", st[j]);
-	}
 	printf("\n");
 }
 
 void load_constant(uint32_t* matrix) {
 	/* load in constant value "expand 32-byte k" */
-	//FIXME: we will have to account for endianness here
 	matrix[0] = 0x61707865;		// "expa"
 	matrix[1] = 0x3320646e;		// "nd 3"
 	matrix[2] = 0x79622d32;		// "2-by"
 	matrix[3] = 0x6b206574;		// "te k"
-
 }
 
 void load_key(uint32_t* matrix, uint32_t* key) {
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 8; i++)
 		matrix[i+4] = conv(key[i]);
-		//printf("%08x ", key[i]);
-		//printf("%08x", matrix[i+4]);
-	}
-	printf("\n");
 }
 
 void load_nonce(uint32_t* matrix, uint32_t* nonce) {
-	for (int i = 0; i < 2; i++) {
+	for (int i = 0; i < 2; i++)
 		matrix[i+14] = nonce[i];
-		printf("%08x", matrix[i+14]);
-	}
 }
 
-//FIXME: create a super function that will take in a key
 void chacha(uint32_t* key, uint32_t* nonce, unsigned char* str, uint32_t ctr) {
 	// allocate an array of 16 words for the 4x4 chacha matrix
 	uint32_t a[16];
+	// set the counter
 	a[12] = ctr;
+	// set the third to last word to 0 (?)
 	a[13] = 0x00000000;
 
 	// load the matrix
@@ -85,21 +77,14 @@ void chacha(uint32_t* key, uint32_t* nonce, unsigned char* str, uint32_t ctr) {
 	load_key(a, key);
 	load_nonce(a, nonce);
 
-/*
-	printf("\nChaCha state:\n");
-	for (int j = 0; j < 16; j++) {
-		printf("%08x\n", a[j]);
-	}
-*/
-
-	printf("\nkey:\n%08x%08x%08x%08x%08x%08x%08x%08x\n", key[0],key[1],key[2],key[3],key[4],key[5],key[6],key[7]);
-
-	printf("nonce:\n%08x%08x\n",conv(a[14]),conv(a[15]));
+	//printf("Key:\n%08x%08x%08x%08x%08x%08x%08x%08x\n", key[0],key[1],key[2],key[3],key[4],key[5],key[6],key[7]);
+	//printf("Nonce:\n%08x%08x\n",conv(a[14]),conv(a[15]));
 
 	// copy input vector
 	uint32_t b[16];
 	for (int j = 0; j < 16; j++)
 		b[j] = a[j];
+	
 	// perform the quarter round function 20 times total (twice per loop)
 	for (int i = 0; i < 10; i++) {
 		// column rounds
@@ -116,74 +101,23 @@ void chacha(uint32_t* key, uint32_t* nonce, unsigned char* str, uint32_t ctr) {
 
 	// add the mixed matrix to the original matrix mod 2^32 to generate the keystream
 	// b now contains 64 bytes of pseudo random data to be used in our stream cipher
-	for (int j = 0; j < 16; j++) {
+	for (int j = 0; j < 16; j++)
 		b[j] += a[j];
-		//printf("%02x", b[j]);
-	}
-	//printf("\n");
 	// ready the keystream buffer with correct index
 	int k = 0 + (ctr*64);
-	printf("\nkeystream:\n");
+	//printf("\nkeystream:\n");
 	for (int i = 0; i < 16; i++) {
 		for (int j = 0; j < 4; j++) {
 			str[k] = b[i] >> ((8*j) & 0xff);
-			printf("%02x", str[k]);
+			//printf("%02x", str[k]);
 			k++;
 		}
 	}
-	printf("\n");
+	//printf("\n");
 }
 
-void chacha_setup(unsigned char* key, unsigned char* nonce) {
-	// FIXME: 
-	printf("Printing key:\n");
-	for (int i = 0; i < 4; i++)
-		printf("%02x", key[i]);
-
-	for (int i = 0; i < 3; i++)
-		printf("%02x", key[i]);
-}
-
-int main(int argc, char** argv) {
-	//char* str = "This is my secret message";
-	size_t sz = strlen(argv[1]);
-	int amt = (sz / 64) + 1;
-	unsigned char* ptr;
-	unsigned char st[sz];
-	
-	// dynamically allocate an array
-	ptr = (unsigned char*)malloc((64*amt)*sizeof(unsigned char));
-	
-	// check to make sure memory was allocated
-	if (!ptr) {
-		printf("Cannot allocate memory!\n");
-		exit(0);
-	}
-
-	uint32_t x[8];
-	int fn = open("/dev/urandom", O_RDONLY);
-	read(fn, x, 32);
-
-	uint32_t y[2];
-	int fn2 = open("/dev/urandom", O_RDONLY);
-	read(fn2, y, 32);
-
-	// generate the keystream (ptr)
-	for (int i = 0; i < amt; i++) {
-		chacha(x, y, ptr, i);
-	}
-	printf("\n");
-	// encrypt the input stream with the keystream
-	for (int j = 0; j < sz/*amt*64*/; j++) {
-		printf("%02x", ptr[j]);
-		st[j] = argv[1][j] ^ ptr[j];
-	}
-	printf("\nKeystream xored with plaintext:\n");
-	for (int j = 0; j < sz; j++) {
-		printf("%02x", st[j]);
-	}
-	printf("\n");
-	// deallocate memory
-	free(ptr);
-	return 0;
+void chacha_stream(uint32_t* key, uint32_t* nonce, unsigned char* string, int blocks) {
+	for (int i = 0; i < blocks; i++)
+		chacha(key, nonce, string, i);
+	// FIXME: append ciphertext to the end of the nonce
 }
